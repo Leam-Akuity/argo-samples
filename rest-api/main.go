549716a70019bb506455b13bc49f785bbd13a91d
Leam Delaney <Leam.Delaney@Akuity.io>
2026-08-12 22:24:32 -0500

    small change to pickup pipeline

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
+			"message": "Hello, World!!",
 		})
 	})
 
